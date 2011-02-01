2d03ad2b4c2a64cf68952d27638f1e7e22eec9e2
Andrey Smirnov <allter@gmail.com>
2011-02-01 10:29:16 +0300

    Added logging from agent

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
+function Agent( id )
+{
+	this.id = id;
+}
+Agent.prototype = {
+	log: function ( msg_html )
+	{
+		var log_element = $('#log');
+		log_element.append( "<b>" + this.id + "</b>" + ": " + ( msg_html || '' ) );
+	}
+};
+
 function hello( frame_names )
 {
 	//alert( frames[ frame_names[0] ] );
 	//alert( document.getElementById( 'src' ) );
-	window.alert('hello: 42');
+
+	var a1 = new Agent( 1 );
+	a1.log();
+
+	//window.alert('hello: 42');
+
+/*
 	for ( i in frame_names )
 	{
 		// TODO: understand JQuery
 		$('#area').append( $('#sample').attr( 'id', frame_names[i] ).html() );
 	}
+*/
 	//$('#f1').attr('style', 'color:red');
 }
